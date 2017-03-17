e158ba8f17c7dfb3acd665210425cee90a19f560
CarloSolis <Cali.1997@aol.com>
2017-03-17 17:49:31 -0600

    UserClass

---
+++ TestJen/src/testjen/TestClass.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjen;
+
+/**
+ *
+ * @author Cali
+ */
+public class TestClass {
+
+    protected int id;
+    protected String name;
+    protected int phone;
+    protected String email;
+    protected String password;
+
+    public TestClass() {
+    }
+
+    public TestClass(int id, String name, int phone, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
